69eeca62abcd43192c0d1ad79082be82b0c2a278
kazutokato <e.t.s.w.a.n.a@gmail.com>
2020-12-16 05:43:18 +0000

    [Update] Debug 5,6,7

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       flash[:notice] = "Book was successfully created."
@@ -44,6 +45,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body, :id)
   end
 end
